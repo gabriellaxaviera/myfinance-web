ad0d4702fbb21aa1d3309afd8f7d32a77d81e283
gabriellaxarruda <axavierga@gmail.com>
2023-03-15 22:05:38 -0300

    move objects and change name

---
+++ my_finance_web_netcore/src/Controllers/TransacaoController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using myfinance_web_netcore.Models;
+
+namespace myfinance_web_netcore.Controllers
+{
+    [Route("[controller]")]
+    public class TransacaoController : Controller
+    {
+        private readonly ILogger<PlanoContaController> _logger;
+
+        public TransacaoController(ILogger<PlanoContaController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("Cadastro")]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Cadastro")]
+        public IActionResult Cadastro(TransacaoModel model)
+        {
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
